70df7017cc8f8d18f627c7dd1d88bd51cf26ecd1
nwalton125 <nwalton125@gmail.com>
2020-05-21 11:38:08 -0700

    Tested system and jimmied with the parameters.

--- verysimpleratingsystem.py
+++ verysimpleratingsystem.py
@@ -1,6 +1,6 @@
 #Players are of the form (name, rating, multiplier)
-def new_player_rating(name):
-	return (1200, 10)
+def new_player_rating():
+	return (1200, 5)
 
 #Sums the integers from a to b inclusive. Not needed in the current implementation.
 def sum_ints(a, b):
@@ -14,7 +14,7 @@ def rate_game(player_dict, game_result):
 	bags = game_result[1]
 	while bags > 0:
 		for player in game_result[0]:
-			new_ratings[player] = (player_dict[player][1] * 100 * (game_result[0][player] / total_score - player_dict[player][0] / total_rating), max(1, player_dict[player][1] - 1))
+			new_ratings[player] = (player_dict[player][0] + player_dict[player][1] * 100 * (game_result[0][player] / total_score - player_dict[player][0] / total_rating), max(1, player_dict[player][1] - 0.5))
 		for player in new_ratings:
 			player_dict[player] = new_ratings[player]
 		bags -= 1
