4e4f31649d1328bf54a2ae1ee8d1c771e75b9a26
nwalton125 <nwalton125@gmail.com>
2020-05-21 11:15:24 -0700

    Update verysimpleratingsystem.py

--- verysimpleratingsystem.py
+++ verysimpleratingsystem.py
@@ -2,7 +2,7 @@
 def new_player_rating(name):
 	return (1200, 10)
 
-#Sums the integers from a to b inclusive.
+#Sums the integers from a to b inclusive. Not needed in the current implementation.
 def sum_ints(a, b):
 	return (b-a) * (b-a+1) // 2
 
@@ -11,12 +11,10 @@ def rate_game(player_dict, game_result):
 	total_rating = sum([player_dict[player][0] for player in game_result[0]])
 	total_score = sum([game_result[0][player] for player in game_result[0]])
 	new_ratings = {}
-	for player in game_result[0]:
-		mpr = 0
-		if player_dict[player][1] >= game_result[1]:
-			mpr = sum_ints(player_dict[player][1] - game_result[1] + 1, player_dict[player][1])
-		else:
-			mpr = sum_ints(1, player_dict[player][1]) + game_result[1] - player_dict[player][1]
-		new_ratings[player] = (multiplier * 100 * (game_result[0][player] / total_score - player_dict[player][0] / total_rating), max(1, player_dict[player][1] - game_result[1]))
-	for player in new_ratings:
-		player_dict[player] = new_ratings[player]
+	bags = game_result[1]
+	while bags > 0:
+		for player in game_result[0]:
+			new_ratings[player] = (player_dict[player][1] * 100 * (game_result[0][player] / total_score - player_dict[player][0] / total_rating), max(1, player_dict[player][1] - 1))
+		for player in new_ratings:
+			player_dict[player] = new_ratings[player]
+		bags -= 1
